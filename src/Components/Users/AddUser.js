7caeb086aff2158b46a6816279f5e699e366b3b8
Nandini <snandiniuday@gmail.com>
2023-03-27 22:09:48 +0530

    changes

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 //components
 import Card from '../UI/Card.js';
+import Button from '../UI/Button.js';
 
 //css files
 import './AddUser.css';
 
 function AddUser() {
+  const [userName, setUserName] = useState('');
+  const [age, setAge]= useState('');
     const handleSubmit =(prop) =>{
         prop.preventDefault();
+        console.log(userName, age);
+    }
+    const handleUserNameChange = (event) => {
+      setUserName(event.target.value);
+    }
+
+    const handleAgeChange = (event) => {
+      setAge(event.target.value);
     }
   return (
     <div>
         <Card className="input">
         <form onSubmit={handleSubmit}>
             <label>User Name:</label>
-            <input type="text" placeholder="Enter Name To Be Added"></input>
+            <input type="text" placeholder="Enter Name To Be Added" onChange={handleUserNameChange}></input>
             <label>Age:</label>
-            <input type="number" placeholder="Enter Age To Be Added"></input>
-            <button type="submit">Add User</button>
+            <input type="number" placeholder="Enter Age To Be Added" onChange={handleAgeChange}></input>
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     </div>
